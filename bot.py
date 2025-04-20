a7f6a07392ecda73975147d0186b188749c83c9a
msandipan25 <msandipan512@gmail.com>
2025-04-20 15:29:10 +0530

    San  bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@sandipanmukherjee11?si=-_0WWE2wfvB2GZXY
 # Ask Doubt on telegram @KingVJ01
 
 from pyrogram import Client
@@ -9,10 +9,10 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "techvj login",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
+            "san login",
+            api_id=26745242,
+            api_hash=e9b17e0b329fa1876b8d92922bf0c00b,
+            bot_token=7129963475:AAH3Ak4FgNsmC01gwSeDfjfrq_yqKvkJPBA,
             plugins=dict(root="TechVJ"),
             workers=50,
             sleep_threshold=10
@@ -22,7 +22,7 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('Bot Started Powered By @nooneisperfect5')
 
     async def stop(self, *args):
 
